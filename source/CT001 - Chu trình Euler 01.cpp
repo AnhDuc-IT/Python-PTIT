46bb5c9b6035509cb29b08bd1558c95ad31969a5
AnhDuc-IT <duckuro.ptit@gmail.com>
2025-09-24 23:13:38 +0700

    hello

--- source/CT001 - Chu trình Euler 01.cpp
+++ source/CT001 - Chu trình Euler 01.cpp
@@ -2,26 +2,33 @@
 using namespace std;
 using ll = long long;
 #define endl "\n"
-#define ii pair <int,int>
-#define iii pair <pair<int,int>>
+#define ii pair<int, int>
+#define iii pair<pair<int, int>>
 int n, m, s, degree[10005], t;
-set <int> adj[10005];
+set<int> adj[10005];
 bool visited[10005];
-void nhap(){
-    if(t == 1){
+void nhap()
+{
+    if (t == 1)
+    {
         cin >> n >> m;
-        for(int i = 0; i < m; i++){
-            int x, y; cin >> x >> y;
+        for (int i = 0; i < m; i++)
+        {
+            int x, y;
+            cin >> x >> y;
             adj[x].insert(y);
             adj[y].insert(x);
             degree[x]++;
             degree[y]++;
         }
     }
-    else{
+    else
+    {
         cin >> n >> m >> s;
-        for(int i = 0; i < m; i++){
-            int x, y; cin >> x >> y;
+        for (int i = 0; i < m; i++)
+        {
+            int x, y;
+            cin >> x >> y;
             adj[x].insert(y);
             adj[y].insert(x);
             degree[x]++;
@@ -29,65 +36,88 @@ void nhap(){
         }
     }
 }
-bool BFS(){
-    queue <int> q;
+bool BFS()
+{
+    queue<int> q;
     q.push(1);
     visited[1] = true;
-    while(!q.empty()){
-        auto x = q.front(); q.pop();
-        for(auto v : adj[x]){
-            if(!visited[v]){
+    while (!q.empty())
+    {
+        auto x = q.front();
+        q.pop();
+        for (auto v : adj[x])
+        {
+            if (!visited[v])
+            {
                 q.push(v);
                 visited[v] = true;
             }
         }
     }
-    for(int i = 1; i <= n; i++){
-        if(!visited[i]) return false;
+    for (int i = 1; i <= n; i++)
+    {
+        if (!visited[i])
+            return false;
     }
     return true;
 }
-int solve(){
-    if(!BFS()) return 0;
+int solve()
+{
+    if (!BFS())
+        return 0;
     int dem = 0;
-    for(int i = 1; i <= n; i++){
-        if(degree[i] % 2) ++dem;
+    for (int i = 1; i <= n; i++)
+    {
+        if (degree[i] % 2)
+            ++dem;
     }
-    if(dem == 0) return 1;
-    else if(dem != 2) return 2;
+    if (dem == 0)
+        return 1;
+    else if (dem != 2)
+        return 2;
     return 0;
 }
-void Euler(int s){
-    stack <int> st;
+void Euler(int s)
+{
+    stack<int> st;
     st.push(s);
-    vector <int> res;
-    while(!st.empty()){
+    vector<int> res;
+    while (!st.empty())
+    {
         auto x = st.top();
-        if(adj[x].size()){
+        if (adj[x].size())
+        {
             int y = *adj[x].begin();
             st.push(y);
             adj[x].erase(y);
             adj[y].erase(x);
         }
-        else{
+        else
+        {
             st.pop();
             res.push_back(x);
         }
     }
     reverse(begin(res), end(res));
-    for(auto x : res){
+    for (auto x : res)
+    {
         cout << x << " ";
     }
+    cout << "Is the change";
 }
-int main(){
-    ios_base :: sync_with_stdio(false);
+int main()
+{
+    ios_base ::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> t;
+    cout << t << endl;
     nhap();
-    if(t == 1){
+    if (t == 1)
+    {
         cout << solve() << endl;
     }
-    else{
+    else
+    {
         Euler(s);
         cout << endl;
     }
